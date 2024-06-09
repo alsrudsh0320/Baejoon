96fc3d177a1c7475dcfeee81721154037c5741c7
alsrudsh0320 <117179278+alsrudsh0320@users.noreply.github.com>
2024-06-10 04:12:50 +0900

    Delete 백준/Bronze/2588. 곱셈 directory remove

--- 백준/Bronze/2588. 곱셈/곱셈.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    val x1 = readln().toInt()               // 1
-    val x2 : String = readln()              // 2
-    println(x2[2].toString().toInt() * x1)  // 3
-    println(x2[1].toString().toInt() * x1)  // 4
-    println(x2[0].toString().toInt() * x1)  // 5
-    println(x2.toInt() * x1)                // 6
-}
