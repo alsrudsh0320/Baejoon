433825d7e5954fb8ef638c5dd05a9830eb2c3065
alsrudsh0320 <117179278+alsrudsh0320@users.noreply.github.com>
2024-06-10 06:27:16 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 324 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.kt
@@ -0,0 +1,29 @@
+import java.util.StringTokenizer
+
+fun main(){
+    val st = StringTokenizer(readln())
+    val N = st.nextToken().toInt()      // row
+    val M = st.nextToken().toInt()      // col
+
+    var matrix1 = Array(N) { IntArray(M) {0} }
+    var matrix2 = Array(N) { IntArray(M) {0} }
+    for(i in 0 until  N){
+        val rowST = StringTokenizer(readln())
+        for(j in 0 until M){
+            matrix1[i][j] = rowST.nextToken().toInt()
+        }
+    }
+    for(i in 0 until  N){
+        val rowST = StringTokenizer(readln())
+        for(j in 0 until M){
+            matrix2[i][j] = rowST.nextToken().toInt()
+        }
+    }
+    // result
+    for(i in 0 until  N){
+        for(j in 0 until M){
+            print("${matrix1[i][j] + matrix2[i][j]} ")
+        }
+        println()
+    }
+}
