9ae8fcda6b065a144767cebe20dac07931125c41
alsrudsh0320 <117179278+alsrudsh0320@users.noreply.github.com>
2024-06-24 01:19:04 +0900

    [Bronze II] Title: OX퀴즈, Time: 88 ms, Memory: 12420 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.kt
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.kt
@@ -1,11 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+
 fun main(){
-    val test_case_num = readln().toInt()
-    val sb = StringBuilder()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val test_case_num = br.readLine().toInt()
 
     repeat(test_case_num){
         var total_score = 0
         var current_score = 0
-        val OXString = readln()
+        val OXString = br.readLine()
 
         OXString.forEach { ch ->
             when(ch){
@@ -18,8 +26,8 @@ fun main(){
                 }
             }
         }
-        sb.append(total_score)
-        sb.append('\n')
+        bw.append("$total_score\n")
     }
-    print(sb)
+    bw.flush()
+    bw.close()
 }
